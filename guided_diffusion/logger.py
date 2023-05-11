80ce54b3f252cfab408f3bd78433dd1e5f3f718f
李桐 <80096418+Li-Tong-621@users.noreply.github.com>
2023-05-11 15:15:16 +0800

    Update logger.py

--- guided_diffusion/logger.py
+++ guided_diffusion/logger.py
@@ -1,7 +1,4 @@
-"""
-Logger copied from OpenAI baselines to avoid extra RL-based dependencies:
-https://github.com/openai/baselines/blob/ea25b9e8b234e6ee1bca43083f8f3cf974143998/baselines/logger.py
-"""
+
 
 import os
 import sys
