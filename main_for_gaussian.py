5ef2260e2bba2c7dd04e49ba89907dc9b170f37c
李桐 <80096418+Li-Tong-621@users.noreply.github.com>
2023-10-14 18:09:10 +0800

    Update main_for_gaussian.py

--- main_for_gaussian.py
+++ main_for_gaussian.py
@@ -8,24 +8,20 @@ import torch.nn as nn
 import torch.utils.data as data
 import torch.backends.cudnn as cudnn
 from guided_diffusion.unet import UNetModel
-import utils_image as util
+import utils.utils_image as util
 from Data import Dataset
-import utils_sampling
+import utils.utils_sampling as utils_sampling
 from collections import OrderedDict
 import os
 import torch.nn.functional as F
-import lpips
-os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-import torch_fidelity
-loss_fn =lpips.LPIPS(net='alex', version='0.1').to('cuda')
+os.environ["CUDA_VISIBLE_DEVICES"] = '2'
+
 
 def data_transform(X):
     return 2 * X - 1.0
 
-
 def data_transform_reverse(X):
     return torch.clamp((X + 1.0) / 2.0, 0.0, 1.0)
-
 
 def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_timesteps):
     def sigmoid(x):
@@ -47,13 +43,11 @@ def get_beta_schedule(beta_schedule, *, beta_start, beta_end, num_diffusion_time
     assert betas.shape == (num_diffusion_timesteps,)
     return betas
 
-
 def MeanUpsample(x, scale):
     n, c, h, w = x.shape
     out = torch.zeros(n, c, h, scale, w, scale).to(x.device) + x.view(n, c, h, 1, w, 1)
     out = out.view(n, c, scale * h, scale * w)
     return out
-
 
 class Train(object):
     def __init__(self, config):
@@ -98,23 +92,19 @@ class Train(object):
         print('____________________________prepare_over____________________________')
 
     def sample_image(self,
-                     x_cond,
                      x,
                      last=True,
                      eta=0.8,
-                     difussion_times=500,
-                     sample_type='DDIM',
-                     travel_length=20, 
-                     travel_repeat=3
+                     difussion_times=500
                      ):
         #______________________embedding for gaussian___________________
 
-        e = torch.randn_like(x_cond[:, :, :, :])
-        t = torch.ones(x_cond.shape[0]).to(self.device)
+        e = torch.randn_like(x[:, :, :, :])
+        t = torch.ones(x.shape[0]).to(self.device)
         t = t*(difussion_times-1)
         t = torch.tensor(t,dtype=torch.int)
         a = (1 - self.betas).cumprod(dim=0).index_select(0, t).view(-1, 1, 1, 1)
-        x_cond = x_cond[:, :, :, :] * a.sqrt() #eq.(3-11)
+        x_N = x[:, :, :, :] * a.sqrt()
         #____________________________  end  ____________________________
 
         
@@ -123,12 +113,11 @@ class Train(object):
         seq=list(seq[:-1])+[difussion_times-1]  #a liilte different from ddim
 
 
-        xs = utils_sampling.generalized_steps(x=x,
-                                                x_cond=x_cond,
-                                                seq=seq,
-                                                model=self.model,
-                                                b=self.betas,
-                                                eta=eta)
+        xs = utils_sampling.generalized_steps(x=x_N,
+                                            seq=seq,
+                                            model=self.model,
+                                            b=self.betas,
+                                            eta=eta)
 
         if last:
             xs = xs[0][-1]
@@ -138,15 +127,7 @@ class Train(object):
                   eta=0.8,
                   eval_time=1,
                   difussion_times=500,
-                  sample_type='DDIM',
-                  travel_length=20, 
-                  travel_repeat=3
                   ):
-
-        avg_rgb_psnr = 0.0
-        avg_lpips=0.0
-        psnr_list = []
-
         idx = 0
         with torch.no_grad():
             for index, (noise, clean) in enumerate(self.val_loader, 0):
@@ -169,13 +150,10 @@ class Train(object):
 
                 for i in range(eval_time):
                     x = torch.randn(b, c, h, w, device=self.device)
-                    denoise += self.sample_image(x_cond=data_transform(noise),
-                                                 x=x,
+                    denoise += self.sample_image(
+                                                 x=data_transform(noise),
                                                  eta=eta,
                                                  difussion_times=difussion_times,
-                                                 sample_type=sample_type,
-                                                 travel_length=travel_length,
-                                                 travel_repeat=travel_repeat
                                                  )
 
                 denoise = data_transform_reverse(denoise / eval_time).to(self.device)
@@ -188,70 +166,53 @@ class Train(object):
 
 
                 for i in range(len(denoise)):
-
-                    
-                    current_lpips_distance = loss_fn.forward(denoise[i, :, :, :], clean[i, :, :, :])
-                    avg_lpips+=current_lpips_distance
-
                     denoise_one = denoise[i, :, :, :]
                     denoise_one = util.tensor2uint(denoise_one)
-
-                    util.imsave(denoise_one, self.config['save']['photo_path'] + str(idx) + '_denoise.png')
+                    util.imsave(denoise_one, self.config['save']['photo_path'] +'denoise/'+ str(idx)+'S_'+str(self.config['diffusion']['sampling_timesteps'])+'_R_'+ str(eval_time)+ '_denoise.png')
 
                     clean_one = util.tensor2uint(clean[i, :, :, :])
-                    util.imsave(clean_one, self.config['save']['photo_path'] + str(idx) + '_clean.png')
+                    util.imsave(clean_one, self.config['save']['photo_path'] +'clean/'+ str(idx) + '_clean.png')
                     
-
                     noise_one = noise[i, :, :, :]
                     noise_one = util.tensor2uint(noise_one)
-
                     util.imsave(noise_one, self.config['save']['photo_path'] + str(idx) + '_noise.png')
 
                     idx = idx + 1
 
-                    current_psnr = util.calculate_psnr(denoise_one, clean_one)
-                    avg_rgb_psnr += current_psnr
+        
+        print('Finish!')
 
-        avg_rgb_psnr = avg_rgb_psnr / idx
-        print('PSNR: ', avg_rgb_psnr)
-        print('Lpips: ', avg_lpips/idx)
-
-        return avg_rgb_psnr
+        
 
 
-
+#You can also change your options here
 option = {
     'data': {
         'test_n_channels': 3,
-        'test_H_size': 256,
+        'test_H_size': 256,                                 #fixed,imagesize for ImageNet
         'test_path_noise':'',
         'test_path_clean':'',
         'test_batch_size': 1,
-        'test_num_workers': 1,
-        'image_size': 256,
-        'test_opt': 'Kodak-list',  #ImageNet / others,
-        'noise_sigma':150
+        'test_num_workers': 1,                              
+        'test_opt': 'Kodak',                                #Dataset
+        'noise_sigma':150                                   #test noise_lvel for Gaussian noise
     },
 
     'model': {
-        'type': "openai",
+        'type': "openai",                                   #fixed
     },
 
     'diffusion': {
-        'beta_schedule': 'linear',
-        'beta_start': 0.0001,
-        'beta_end': 0.02,
-        'num_diffusion_timesteps': 1000,
-        'sampling_timesteps': 1
-    },
-
-    'training': {
-        'n_epochs': 200000,
+        'beta_schedule': 'linear',                          #fixed
+        'beta_start': 0.0001,                               #fixed
+        'beta_end': 0.02,                                   #fixed
+        'num_diffusion_timesteps': 1000,                    #fixed
+        'sampling_timesteps': 1                             #fixed
     },
 
     'save': {
-        'photo_path': './results/photo_temp/',
-        'ddpm_checkpoint': './pre-trained/256x256_diffusion_uncond.pt',
+        'photo_path': './results/photo_temp/',  
+        'ddpm_checkpoint': './pre-trained/256x256_diffusion_uncond.pt'          #the path of the pre-trained diffusion model
     },
 
     'device_id': 'cuda',
@@ -259,15 +220,14 @@ option = {
 import argparse
 parser = argparse.ArgumentParser(description='Gasussian Color Denoising using DMID')
 
-parser.add_argument('--data_path', default='./data/Imagenet', type=str, help='for example: ./data/Imagenet')
-parser.add_argument('--dataset', default='ImageNet', type=str, help='ImageNet/CBSD68/Kodak24/McMaster/...')
+parser.add_argument('--data_path', default='/data/litong/litong/Dataset/denoise_data/benchmark/McMaster', type=str, help='for example: ./data/Imagenet')
+parser.add_argument('--dataset', default='McMaster', type=str, help='ImageNet/CBSD68/Kodak24/McMaster/...')
 parser.add_argument('--test_sigma', default=250, type=int, help='50/100/150/200/250/...')
 parser.add_argument('--S_t', default=1, type=int, help='sampling times in one inference')
-parser.add_argument('--R_t', default=1, type=int, help='inference times')
+parser.add_argument('--R_t', default=1, type=int, help='repetition times of multiple inferences')
 args = parser.parse_args()
 
-d_t_list=[115,215,291,347,393]
-s_t_list=[2,2,2,2,2]
+N_list=[115,215,291,347,393]
 noise_sigma_list=[50,100,150,200,250]
 
 option['data']['test_path_noise'] = args.data_path
@@ -275,17 +235,18 @@ option['data']['test_path_clean'] = args.data_path
 option['data']['test_opt'] = args.dataset
 option['data']['noise_sigma']=args.test_sigma
 option['diffusion']['sampling_timesteps']=args.S_t
-N=d_t_list[ noise_sigma_list.index(args.test_sigma) ]
+N=N_list[ noise_sigma_list.index(args.test_sigma) ]
 os.makedirs('./results/'+option['data']['test_opt'],exist_ok=True)
+os.makedirs('./results/'+option['data']['test_opt']+'/clean'  ,exist_ok=True)
+os.makedirs('./results/'+option['data']['test_opt']+'/denoise',exist_ok=True)
 option['save']['photo_path']='./results/'+option['data']['test_opt']+'/'
-print(option)
-
 
 TRAIN=Train(config=option)
-TRAIN.eval_ddpm(eta=0.85,
+print(option)
+
+TRAIN.eval_ddpm(
                 eval_time=args.R_t,
                 difussion_times=N,
-                sample_type='DDIM'
                 )
 
 
