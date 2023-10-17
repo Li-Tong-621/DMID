320cdcdc605e19323e46761ada874c6c05ecde25
李桐 <80096418+Li-Tong-621@users.noreply.github.com>
2023-10-17 21:53:50 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,4 +1,5 @@
 #If you want to reproduce quickly, you can reduce the repetitions times R_t. For example, setting R_ T=1 or R_ T=10, the approximate results can be obtained.
+
 #TABLE 1, Classical Gaussian image denoising:
 
 #CBSD68
@@ -18,19 +19,18 @@ python main_for_gaussian.py --data_path './data/Urban100' --dataset 'Urban100' -
 python main_for_gaussian.py --data_path './data/Urban100' --dataset 'Urban100' --test_sigma 25 --S_t 1 --R_t 1
 python main_for_gaussian.py --data_path './data/Urban100' --dataset 'Urban100' --test_sigma 50 --S_t 1 --R_t 1
 
+
 #TABLE 3, real-world image denoising:
 
-##cc-d
+#cc-d
 python main_for_real.py --clean_path '' --noisy_path './data/CC-full/Noisy/' --datatype 'CC' --pertrianed './pre-trained/CC.pt' --S_t 2 --R_t 500
 #cc-p:
 python main_for_real.py --clean_path '' --noisy_path './data/CC-full/Noisy/' --datatype 'CC' --pertrianed './pre-trained/CC.pt' --S_t 1 --R_t 1
-
-##polyu-d
+#polyu-d
 python main_for_real.py --clean_path '' --noisy_path '' --datatype 'PolyU' --pertrianed './pre-trained/PolyU.pt' --S_t 3 --R_t 333
 #polyu-p:
 python main_for_real.py --clean_path '' --noisy_path '' --datatype 'PolyU' --pertrianed './pre-trained/PolyU.pt' --S_t 1 --R_t 1
-
-##fmdd-d
+#fmdd-d
 python main_for_real.py --clean_path '' --noisy_path '' --datatype 'FMDD' --pertrianed './pre-trained/FMDD.pt' --S_t 3 --R_t 333
 #fmdd-p:
 python main_for_real.py --clean_path '' --noisy_path '' --datatype 'FMDD' --pertrianed './pre-trained/FMDD.pt' --S_t 2 --R_t 1
