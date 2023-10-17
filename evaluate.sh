27015a0fd5a67ebdabe650c409bfaeda99842e89
李桐 <80096418+Li-Tong-621@users.noreply.github.com>
2023-10-17 21:46:51 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,1 +1,22 @@
 
+
+
+
+
+
+
+##cc-d
+python main_for_real.py --clean_path './data/CC-full/GT/' --noisy_path './data/CC-full/Noisy/' --datatype 'CC' --pertrianed './pre-trained/CC.pt' --S_t 2 --R_t 500
+#cc-p:
+python main_for_real.py --clean_path './data/CC-full/GT/' --noisy_path './data/CC-full/Noisy/' --datatype 'CC' --pertrianed './pre-trained/CC.pt' --S_t 1 --R_t 1
+
+##polyu-d
+python main_for_real.py --clean_path '' --noisy_path '' --datatype 'PolyU' --pertrianed './pre-trained/PolyU.pt' --S_t 3 --R_t 333
+#polyu-p:
+python main_for_real.py --clean_path '' --noisy_path '' --datatype 'PolyU' --pertrianed './pre-trained/PolyU.pt' --S_t 1 --R_t 1
+
+##fmdd-d
+python main_for_real.py --clean_path '' --noisy_path '' --datatype 'FMDD' --pertrianed './pre-trained/FMDD.pt' --S_t 3 --R_t 333
+#fmdd-p:
+python main_for_real.py --clean_path '' --noisy_path '' --datatype 'FMDD' --pertrianed './pre-trained/FMDD.pt' --S_t 2 --R_t 1
+
