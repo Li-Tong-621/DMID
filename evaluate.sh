dcdbe3ff1096ed8ff69585e86cccae46896ca4a4
李桐 <80096418+Li-Tong-621@users.noreply.github.com>
2023-10-17 21:50:21 +0800

    Update evaluate.sh

--- evaluate.sh
+++ evaluate.sh
@@ -1,9 +1,11 @@
+#If you want to reproduce quickly, you can reduce the repetitions times R_t. For example, setting R_ T=1 or R_ T=10, the approximate results can be obtained.
+#TABLE 1, Classical Gaussian image denoising:
 
 
 
 
 
-
+#TABLE 3, real-world image denoising:
 
 ##cc-d
 python main_for_real.py --clean_path './data/CC-full/GT/' --noisy_path './data/CC-full/Noisy/' --datatype 'CC' --pertrianed './pre-trained/CC.pt' --S_t 2 --R_t 500
